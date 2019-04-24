794dfd627fcf02670aa8f7a95730bb430cd807a2
Yan Li <yanli@tuneup.ai>
2019-04-23 18:04:10 -0700

    First NGINX setter implementation

---
+++ tuclient_extensions/nginx.py
@@ -0,0 +1,32 @@
+"""NGINX Setter"""
+# Copyright (c) 2017-2019 Yan Li, TuneUp.ai <yanli@tuneup.ai>.
+# All rights reserved.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see
+# https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__author__ = 'Yan Li'
+__copyright__ = 'Copyright (c) 2017-2019 Yan Li, TuneUp.ai <yanli@tuneup.ai>. All rights reserved.'
+__license__ = 'LGPLv2.1'
+__docformat__ = 'reStructuredText'
+
+import os
+import signal
+from tuclient_extensions import get_pids
+
+
+def reload_nginx_config_file():
+    for pid in get_pids('bin/nginx'):
+        os.kill(pid, signal.SIGUSR1)
+
